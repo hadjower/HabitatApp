b74e7dc67fe157820daddaf9fe3ac531f59605ef
hadjower <vladdudars@gmail.com>
2019-01-28 15:05:32 +0200

    Initial commit

---
+++ app/src/main/java/com/hadjower/android/habitatapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hadjower.android.habitatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
